6c7db3f147e3a4ecf5b81ada2dabff52db1b95cd
Maciej Kopeć <kontakt@maciejkopec.pl>
2020-08-08 01:14:50 -0700

    image dark background fix

--- src/components/ImagesGallery.js
+++ src/components/ImagesGallery.js
@@ -1,22 +1,29 @@
 import Container from "@material-ui/core/Container";
 import Grid from "@material-ui/core/Grid";
+import { makeStyles } from "@material-ui/core/styles";
 import Image from "material-ui-image";
 import PropTypes from "prop-types";
 import * as React from "react";
 
 const API_URL = process.env.GATSBY_API_URL;
 
+const useStyles = makeStyles(theme => ({
+  background: theme.palette.background.default,
+}));
+
 export default function ImagesGallery(props) {
   const { items } = props;
+  const classes = useStyles();
+
   return (
-    <Container maxWidth={"md"}>
-      <Grid container justify="center" alignItems="center" spacing={5}>
+    <Container maxWidth={"lg"}>
+      <Grid container justify="center" alignItems="center" spacing={10}>
         {items.map((item, index) => (
-          <Grid item xs={6} md={4} key={index}>
+          <Grid item xs={6} md={2} key={index}>
             <Image
               src={API_URL + item.src}
-              color={"#fafafa"}
-              imageStyle={{ height: "auto" }}
+              color={classes.background}
+              imageStyle={{ height: "100%" }}
               alt={item.alt}
             />
           </Grid>
