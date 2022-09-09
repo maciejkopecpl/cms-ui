21b644cc9bfb55b5a6bd21acbe8caf699e67d718
Maciej Kopeć <kontakt@maciejkopec.pl>
2022-09-08 22:34:31 -0400

    Update deps, add e2e test

---
+++ cypress/e2e/basic.cy.js
@@ -0,0 +1,19 @@
+describe('e2e tests', () => {
+  beforeEach(() => {
+    cy.visit('/')
+  })
+
+  it('displays two leads', () => {
+
+    cy.get('main .react-parallax h1').should('have.text', "Software Engineer")
+    cy.get('main .react-parallax h2').should('have.text', "Maciej Kopeć")
+
+  })
+
+  it('displays `What I do...` second', () => {
+
+    cy.get('main > div:nth-child(2) h2').should('have.text', "What I do...")
+
+  })
+
+})
