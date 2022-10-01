c375b15651bef76e7298ed1e5cc7d5dc22bf5571
Maciej Kopeć <kontakt@maciejkopec.pl>
2022-09-30 22:37:08 -0400

    Replace React.lazy with @loadable

--- src/utils/factories.js
+++ src/utils/factories.js
@@ -6,14 +6,15 @@ import {
   Trophy,
   UnfoldMoreVertical,
 } from "mdi-material-ui/light/index.es";
-import React, { lazy } from "react";
+import React from "react";
+import loadable from "@loadable/component";
 
-const Header = lazy(() => import("../components/Header"));
-const IconsGallery = lazy(() => import("../components/IconsGallery"));
-const ImagesGallery = lazy(() => import("../components/ImagesGallery"));
-const Skills = lazy(() => import("../components/Skills"));
-const WideImage = lazy(() => import("../components/WideImage"));
-const ContactForm = lazy(() => import("../components/ContactForm"));
+const Header = loadable(() => import("../components/Header"));
+const IconsGallery = loadable(() => import("../components/IconsGallery"));
+const ImagesGallery = loadable(() => import("../components/ImagesGallery"));
+const Skills = loadable(() => import("../components/Skills"));
+const WideImage = loadable(() => import("../components/WideImage"));
+const ContactForm = loadable(() => import("../components/ContactForm"));
 
 export const buildComponent = component => {
   const data = JSON.parse(component.data);
